b36079796cfcfd65ca095cf004514d494fe25f28
goll <joaog.bastos@protonmail.ch>
2025-12-12 18:19:39 -0300

    Remove tool-specific files and fix .gitignore

--- app/src/main/java/com/gesac/disciplinas/Main.java
+++ app/src/main/java/com/gesac/disciplinas/Main.java
@@ -4,6 +4,9 @@ public class Main {
     public static void main(String[] args) {
         String banner = """
             :: Sistema de Gestão Acadêmico :: Disciplinas e Turmas
+
+            1. Gerenciar disciplinas
+            2. Calcular a média de uma disciplina
         """;
 
         System.out.println(banner);
